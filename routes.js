cceebe8c4f9d84acf972cfa1d93836112858114e
Do Duc Huy <stormddh@gmail.com>
2021-11-01 18:33:12 +0100

    Add Kibana

--- routes.js
+++ routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const router = express.Router();
 const multer = require('multer')
 const fs = require('fs'),
-    PDFParser = require("pdf2json");
+PDFParser = require("pdf2json");
 
+const router = express.Router();
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './uploads');
